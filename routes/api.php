e0d206acfb0abb45ef89b14aa850ee2a69485672
mimimoonlight <imane.BLK00@gmail.com>
2026-01-30 18:07:14 +0100

    modif

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::middleware(['auth:sanctum','role:admin'])->get('/chef_departement/dashboard',function(){
         return response()->json(['message'=>'Welcome admin']);
     });
